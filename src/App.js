ae24e6ddf6648a9492c76338154d01c832298885
John <johnartuz@hotmail.com>
2021-12-22 00:27:17 -0500

    Cryptopunk Clone with React.js & Web 3.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import CollectionCard from "./components/CollectionCard";
+import Header from "./components/Header";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Punklist from "./components/Punklist";
+import Main from "./components/Main";
 
 function App() {
+  const [punkListData, setPunkListData] = useState([]);
+  const [selectedPunk, setSelectedPunk] = useState(0);
+
+  useEffect(() => {
+    const getMyNfts = async () => {
+      const openseaData = await axios.get("https://testnets-api.opensea.io/assets?asset_contract_address=0x266b3202e0565B4dE36201981770200c7BBE385e&order_direction=asc");
+      console.log(openseaData.data.assets);
+      setPunkListData(openseaData.data.assets);
+    };
+    return getMyNfts();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      {punkListData.length > 0 && (
+        <>
+          <Main punkListData={punkListData} selectedPunk={selectedPunk} />
+          <Punklist punkListData={punkListData} setSelectedPunk={setSelectedPunk} />
+        </>
+      )}
     </div>
   );
 }
